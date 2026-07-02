756f87a9a61f5b6246b9fc4b8177087f89137849
Muhammad Habib <habibkahlon@gmail.com>
2026-07-03 00:54:33 +0500

    rate limiting 2.0 Update youtube.py

--- Backend/routers/youtube.py
+++ Backend/routers/youtube.py
@@ -1,6 +1,6 @@
 import os, httpx
 from fastapi import APIRouter, HTTPException, Request
-from slowapi import Limiter
+from limiter import limiter
 from slowapi.util import get_remote_address
 limiter = Limiter(key_func=get_remote_address)
 from pydantic import BaseModel, Field, field_validator
